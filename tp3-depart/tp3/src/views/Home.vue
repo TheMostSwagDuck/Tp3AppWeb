55a4b90b5e732704a44cf8e335d21bdd38921c88
Gabriel Bertrand <gabriel_bertrand@hotmail.com>
2022-05-05 10:59:04 -0400

    avancement de la vue avec la map

--- tp3-depart/tp3/src/views/Home.vue
+++ tp3-depart/tp3/src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-container fluid>
+      <b-row>
+        <b-col style="margin-left: 50px">
+          <Selection @changeSelectedTrail="onChangeSelectedTrail"/>
+        </b-col>
+        <b-col cols="7" style="margin-right: 50px">
+          <Map v-bind:trail="selectedTrail"/>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Map from '@/components/Map.vue'
+import Selection from '@/components/Selection.vue'
 
 export default {
-  name: 'Home',
   components: {
-    HelloWorld
+    Map,
+    Selection
+  },
+  methods: {
+    onChangeSelectedTrail (newTrail) {
+      this.selectedTrail = newTrail
+    }
+  },
+  data () {
+    return {
+      selectedTrail: null
+    }
   }
 }
 </script>
